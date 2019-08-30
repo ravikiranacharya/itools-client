ae698c7dce3609022e1285819a2f43e065392c83
Ravi Kiran <raviraaga1992@gmail.com>
2019-08-30 13:00:52 +0530

    Refactored fundManagerAnalytics.js

--- src/services/fundManagerAnalytics.js
+++ src/services/fundManagerAnalytics.js
@@ -1,46 +1,32 @@
-import axios from "axios";
+import httpHelpers from "./utils/httpHelpers";
+import config from "../config.json";
 
-const apiUrl = "https://localhost:44311/api/";
-const controller = "fundmanagers";
+const apiEndpoint = `${config.apiUrl}/fundmanagers`;
 
 export async function fetchAllFundManagers() {
-  const data = await axios.get(`${apiUrl}${controller}`);
-  return data.data;
+  return httpHelpers.get(apiEndpoint);
 }
 
 export async function fetchFundManagerOverview(fundManagerId) {
-  const data = await axios.get(
-    `${apiUrl}${controller}/overview/${fundManagerId}`
-  );
-  return data.data;
+  return httpHelpers.get(`${apiEndpoint}/overview/${fundManagerId}`);
 }
 
 export async function fetchSchemesManaged(fundManagerId) {
-  const data = await axios.get(
-    `${apiUrl}${controller}/schemes/${fundManagerId}`
-  );
-  return data.data;
+  return httpHelpers.get(`${apiEndpoint}/schemes/${fundManagerId}`);
 }
 
 export async function fetchRiskReturnStatistics(fundManagerId) {
-  const data = await axios.get(
-    `${apiUrl}${controller}/riskreturnstatistics/${fundManagerId}`
+  return httpHelpers.get(
+    `${apiEndpoint}/riskreturnstatistics/${fundManagerId}`
   );
-  return data.data;
 }
 
 export async function fetchPeerSchemePerformance(fundManagerId) {
-  const data = await axios.get(
-    `${apiUrl}${controller}/peerperformance/${fundManagerId}`
-  );
-  return data.data;
+  return httpHelpers.get(`${apiEndpoint}/peerperformance/${fundManagerId}`);
 }
 
 export async function fetchFundManagerPerformanceOverTime(fundManagerId) {
-  const data = await axios.get(
-    `${apiUrl}${controller}/performance/${fundManagerId}`
-  );
-  return data.data;
+  return httpHelpers.get(`${apiEndpoint}/performance/${fundManagerId}`);
 }
 
 export default {
