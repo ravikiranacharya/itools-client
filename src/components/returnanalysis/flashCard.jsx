06c0e9b171c512a3a2769f020d097c75b12a7756
Ravi Kiran <raviraaga1992@gmail.com>
2019-09-05 15:04:12 +0530

    Added Flashcard component

--- src/components/returnanalysis/flashCard.jsx
+++ src/components/returnanalysis/flashCard.jsx
@@ -3,7 +3,34 @@ import React, { Component } from "react";
 class FlashCard extends Component {
   state = {};
   render() {
-    return <h6>Flash Card</h6>;
+    return (
+      <div className="card shadow flash-card animated--fade-in">
+        <div className="card-header shadow text-left">
+          <div className="row">
+            <div className="col-md-8 col-lg-8 col-sm-8 col-8">
+              <div>HDFC Hybrid Debt - G</div>
+            </div>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="row">
+            <div className="col-md-8 col-lg-8 col-sm-8 col-8">
+              <div className="scheme-returns text-left">
+                <div className="small scheme-category">
+                  Category: <strong>Hybrid</strong>
+                </div>
+                <p className="small returns-date">
+                  21st Jan-2018 to 21st Jan-2019
+                </p>
+              </div>
+            </div>
+            <div className="col-md-4 col-lg-4 col-sm-4 col-4">
+              <h6 className="font-weight-bold">-6.35%</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/returnanalysis/returnAnalytics.jsx
+++ src/components/returnanalysis/returnAnalytics.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import SearchBar from "./searchBar";
 import { fetchAllSchemes } from "../../services/returnAnalytics";
 import Spinner from "../common/spinner";
+import FlashCard from "./flashCard";
 
 class ReturnAnalytics extends Component {
   state = {};
@@ -16,15 +17,22 @@ class ReturnAnalytics extends Component {
 
     const { schemes } = this.state;
     return (
-      <div className="row">
-        <div className="col-md-4 col-lg-4 col-sm-8 col-8">
-          <div className="row">
-            <div className="col-md-10 col-lg-10 col-sm-10 col-10 pr-0">
-              <SearchBar data={schemes}></SearchBar>
+      <div>
+        <div className="row">
+          <div className="col-md-4 col-lg-4 col-sm-8 col-8">
+            <div className="row">
+              <div className="col-md-10 col-lg-10 col-sm-10 col-10 pr-0">
+                <SearchBar data={schemes}></SearchBar>
+              </div>
+              <div className="col-md-2 col-lg-2 col-sm-2 col-2 p-2">
+                <button className="btn btn-success add-schemes-btn">+</button>
+              </div>
             </div>
-            <div className="col-md-2 col-lg-2 col-sm-2 col-2 p-2">
-              <button className="btn btn-success add-schemes-btn">+</button>
-            </div>
+          </div>
+        </div>
+        <div className="row clearfix">
+          <div className="col-md-3 col-lg-3 col-sm-6 col-12 p-2">
+            <FlashCard></FlashCard>
           </div>
         </div>
       </div>
