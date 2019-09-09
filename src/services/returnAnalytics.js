5618a731a7fdfca85d2d008b44526a691d5fbb41
Ravi Kiran <raviraaga1992@gmail.com>
2019-09-09 10:01:01 +0530

    Added more data for schemes to search

--- src/services/returnAnalytics.js
+++ src/services/returnAnalytics.js
@@ -1,14 +1,144 @@
 export async function fetchAllInstruments() {
   return [
-    { instrumentId: 1, instrumentName: "HDFC Hybrid Debt - G" },
-    { instrumentId: 2, instrumentName: "HDFC  Inc -D Reinvest" },
+    { instrumentId: 474, instrumentName: "Axis Banking & PSU Debt -G" },
+    { instrumentId: 475, instrumentName: "Axis Gilt -G" },
+    { instrumentId: 476, instrumentName: "Axis CPOF-1-G" },
+    { instrumentId: 477, instrumentName: "Axis CPOF-2-G" },
+    { instrumentId: 478, instrumentName: "Axis CPOF-3-G" },
+    { instrumentId: 479, instrumentName: "Axis CPOF-4-G" },
+    { instrumentId: 480, instrumentName: "Axis CPOF-5-G" },
+    { instrumentId: 481, instrumentName: "Axis Dynamic Bond -G" },
+    { instrumentId: 482, instrumentName: "Axis Bluechip -G" },
+    { instrumentId: 483, instrumentName: "Axis Focused 25 -G" },
+    { instrumentId: 489, instrumentName: "Axis FTP-34 (392 D)-Reg-G" },
+    { instrumentId: 490, instrumentName: "Axis Gold Exchange Traded " },
+    { instrumentId: 491, instrumentName: "Axis Gold -G" },
+    { instrumentId: 492, instrumentName: "Axis Hybrid -1-G" },
+    { instrumentId: 493, instrumentName: "Axis Hybrid -2-G" },
+    { instrumentId: 494, instrumentName: "Axis Hybrid -3- G" },
+    { instrumentId: 495, instrumentName: "Axis Strategic Bond -G" },
+    { instrumentId: 496, instrumentName: "Axis Reg Saver -G" },
+    { instrumentId: 497, instrumentName: "Axis Liquid -G" },
+    { instrumentId: 498, instrumentName: "Axis LT Equity -G" },
+    { instrumentId: 499, instrumentName: "Axis Midcap -G" },
+    { instrumentId: 500, instrumentName: "Axis STP-G" },
+    { instrumentId: 501, instrumentName: "Axis Treasury Adv -G" },
+    { instrumentId: 504, instrumentName: "Baroda Hybrid Equity -G" },
     {
-      instrumentId: 3,
-      instrumentName: "Aditya Birla Sun Life Tax Relief 96-DR-Dir"
+      instrumentId: 505,
+      instrumentName: "Baroda Banking and Financial Services -G"
     },
-    { instrumentId: 4, instrumentName: "ICICI Prudential AA -D" },
-    { instrumentId: 5, instrumentName: "DHFL Pramerica Arbi -Annual D-Dir" },
-    { instrumentId: 6, instrumentName: "Reliance Inc - G" }
+    { instrumentId: 506, instrumentName: "Baroda Dynamic Bond -G" },
+    { instrumentId: 507, instrumentName: "Baroda Equity Linked Saving  96-D" },
+    { instrumentId: 509, instrumentName: "Baroda Gilt -G" },
+    { instrumentId: 510, instrumentName: "Baroda Multi Cap -G" },
+    { instrumentId: 511, instrumentName: "Baroda Pioneer  Inc -G" },
+    { instrumentId: 512, instrumentName: "Baroda Large Cap -G" },
+    { instrumentId: 513, instrumentName: "Baroda Liquid -Reg-G" },
+    { instrumentId: 514, instrumentName: "Baroda Cons Hybrid -G" },
+    {
+      instrumentId: 515,
+      instrumentName: "Baroda Pioneer Public Sector Undertaking(PSU) Bond -G"
+    },
+    { instrumentId: 516, instrumentName: "Baroda Mid-cap -G" },
+    { instrumentId: 517, instrumentName: "Baroda ST Bond -G" },
+    { instrumentId: 518, instrumentName: "Baroda Treasury Adv -Reg-G" },
+    {
+      instrumentId: 519,
+      instrumentName: "Aditya Birla Sun Life Equity Hybrid 95 -G"
+    },
+    {
+      instrumentId: 520,
+      instrumentName: "Aditya Birla Sun Life Equity Adv -D"
+    },
+    { instrumentId: 521, instrumentName: "Aditya Birla Sun Life AAF-Aggr-G" },
+    { instrumentId: 522, instrumentName: "Aditya Birla Sun Life AAF-Cons-G" },
+    {
+      instrumentId: 523,
+      instrumentName: "Aditya Birla Sun Life AAF-Moderate-G"
+    },
+    { instrumentId: 524, instrumentName: "Aditya Birla Sun Life Buy India -G" },
+    {
+      instrumentId: 525,
+      instrumentName: "Aditya Birla Sun Life Low Duration -G"
+    },
+    {
+      instrumentId: 526,
+      instrumentName: "Aditya Birla Sun Life Liquid -Retail-G"
+    },
+    {
+      instrumentId: 527,
+      instrumentName: "Birla Sun Life CEF-Global Multi Commodity-Retail-G "
+    },
+    {
+      instrumentId: 528,
+      instrumentName: "Birla Sun Life CEF-Global Precious Metal-Retail-G "
+    },
+    { instrumentId: 529, instrumentName: "Aditya Birla Sun Life CPOF-10" },
+    { instrumentId: 530, instrumentName: "Aditya Birla Sun Life CPOF-11" },
+    { instrumentId: 531, instrumentName: "Aditya Birla Sun Life CPOF-12" },
+    { instrumentId: 532, instrumentName: "Aditya Birla Sun Life CPOF-13" },
+    { instrumentId: 533, instrumentName: "Aditya Birla Sun Life CPOF-14" },
+    { instrumentId: 534, instrumentName: "Birla Sun Life CPOF-2 " },
+    { instrumentId: 535, instrumentName: "Birla Sun Life CPOF-3 " },
+    { instrumentId: 536, instrumentName: "Birla Sun Life CPOF-4 " },
+    { instrumentId: 537, instrumentName: "Birla Sun Life CPOF-5 " },
+    { instrumentId: 538, instrumentName: "Birla Sun Life CPOF-6 " },
+    { instrumentId: 539, instrumentName: "Aditya Birla Sun Life CPOF-7" },
+    { instrumentId: 540, instrumentName: "Birla Sun Life CPOF-8 " },
+    { instrumentId: 541, instrumentName: "Aditya Birla Sun Life CPOF-9" },
+    { instrumentId: 542, instrumentName: "Aditya Birla Sun Life D Yield -G" },
+    {
+      instrumentId: 543,
+      instrumentName: "Aditya Birla Sun Life Dynamic Bond -Reg-G"
+    },
+    { instrumentId: 544, instrumentName: "Aditya Birla Sun Life Arbi -G" },
+    { instrumentId: 545, instrumentName: "Aditya Birla Sun Life Equity -G" },
+    {
+      instrumentId: 546,
+      instrumentName: "Aditya Birla Sun Life Floating Rate -Retail-G"
+    },
+    {
+      instrumentId: 547,
+      instrumentName: "Aditya Birla Sun Life Money Manager -Retail-G"
+    },
+    {
+      instrumentId: 548,
+      instrumentName: "Aditya Birla Sun Life Frontline Equity -G"
+    },
+    { instrumentId: 549, instrumentName: "Aditya Birla Sun Life FTP-DA-G" },
+    { instrumentId: 550, instrumentName: "Birla Sun Life FTP-DP-G " },
+    { instrumentId: 551, instrumentName: "Birla Sun Life FTP-DU-G " },
+    { instrumentId: 552, instrumentName: "Birla Sun Life FTP-DV-G " },
+    { instrumentId: 553, instrumentName: "Birla Sun Life FTP-DW-G " },
+    { instrumentId: 554, instrumentName: "Birla Sun Life FTP-EC-G " },
+    { instrumentId: 555, instrumentName: "Aditya Birla Sun Life FTP-ED-G" },
+    { instrumentId: 556, instrumentName: "Aditya Birla Sun Life FTP-EH-G" },
+    { instrumentId: 557, instrumentName: "Birla Sun Life FTP-EJ-G " },
+    { instrumentId: 558, instrumentName: "Birla Sun Life FTP-EL-G " },
+    { instrumentId: 559, instrumentName: "Aditya Birla Sun Life FTP-EN-G" },
+    { instrumentId: 560, instrumentName: "Birla Sun Life FTP-EO-G " },
+    { instrumentId: 561, instrumentName: "Aditya Birla Sun Life FTP-ER-G" },
+    { instrumentId: 562, instrumentName: "Birla Sun Life FTP-ES-G " },
+    { instrumentId: 563, instrumentName: "Birla Sun Life FTP-ET-G " },
+    { instrumentId: 564, instrumentName: "Birla Sun Life FTP-EU-G " },
+    { instrumentId: 565, instrumentName: "Birla Sun Life FTP-EV-G " },
+    { instrumentId: 566, instrumentName: "Birla Sun Life FTP-EW-G " },
+    { instrumentId: 567, instrumentName: "Birla Sun Life FTP-EX-G " },
+    { instrumentId: 568, instrumentName: "Birla Sun Life FTP-EY-G " },
+    { instrumentId: 569, instrumentName: "Aditya Birla Sun Life FTP-EZ-G" },
+    { instrumentId: 570, instrumentName: "Birla Sun Life FTP-FA-G " },
+    { instrumentId: 571, instrumentName: "Aditya Birla Sun Life FTP-FB-G" },
+    { instrumentId: 572, instrumentName: "Birla Sun Life FTP-FC-G " },
+    { instrumentId: 573, instrumentName: "Birla Sun Life FTP-FD-G " },
+    { instrumentId: 574, instrumentName: "Aditya Birla Sun Life FTP-FE-G" },
+    { instrumentId: 575, instrumentName: "Aditya Birla Sun Life FTP-FF-G" },
+    { instrumentId: 576, instrumentName: "Birla Sun Life FTP-FH-G " },
+    { instrumentId: 577, instrumentName: "Birla Sun Life FTP-FI-G " },
+    { instrumentId: 578, instrumentName: "Birla Sun Life FTP-FJ-G " },
+    { instrumentId: 579, instrumentName: "Birla Sun Life FTP-FK-G " },
+    { instrumentId: 580, instrumentName: "Birla Sun Life FTP-FL-G " },
+    { instrumentId: 581, instrumentName: "Birla Sun Life FTP-FM-G " }
   ];
 }
 
